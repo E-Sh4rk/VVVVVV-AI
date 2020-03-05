f026b9aa40f327f4bc065234739ed8e463fb40c4
Mickael Laurent <esh4rk@gmail.com>
2020-03-05 17:58:51 -0600

    improve debug mode
    Former-commit-id: b61766f4c08d989f324bc986092a1193003eef00

--- bot/src/debug.jl
+++ bot/src/debug.jl
@@ -37,51 +37,31 @@ function print_all_projectiles(simulation, truth)
 end
 
 previous_truth = nothing
+previous_simulation = nothing
 function check_simulation(simulation::GameState, truth::GameState)
-    global previous_truth
-    prev = previous_truth
+    global previous_truth, previous_simulation
+    truth_prev = previous_truth
+    sim_prev = previous_simulation
     previous_truth = truth
+    previous_simulation = simulation
 
     if simulation.terminal
         if !truth.terminal
             println("(The predicted game state is wrongly terminal)")
-            print_object(simulation.player, "Player")
             # return false
         end
         return true
     end
+    # PLAYER POSITION
     if !are_game_objects_equivalent(simulation.player, truth.player)
         println("Player position is wrong.")
         print_boxes(simulation.player, truth.player)
         return false
     end
-    if truth.terminal
-        println("The predicted game state should be terminal!")
-        truth_proj = get_proj_in_collision(truth.player, truth.projectiles)
-        if truth_proj == nothing
-            println("The collision check seems to be broken.")
-            (truth_proj,_) = nearest_projectile(player_proj_dist², truth)
-            print_object(truth_proj, "Projectile")
-            print_object(simulation.player, "Player")
-            print_all_projectiles(nothing, truth)
-        else
-            (sim_proj,_) = nearest_projectile(player_proj_dist², simulation)
-            if !are_game_objects_equivalent(sim_proj, truth_proj)
-                println("No projectile at this position in the simulation.")
-                print_boxes(sim_proj, truth_proj)
-                print_object(simulation.player, "Player")
-                print_all_projectiles(simulation, truth)
-            else
-                println("This line should not appear.")
-                print_object(sim_proj, "Projectile")
-                print_object(simulation.player, "Player")
-            end
-        end
-        return false
-    end
+    # PROJECTILES
     for p in simulation.projectiles
         if (p.x > PROJ_DELETED_RIGHT && p.xs > 0) || (p.x < PROJ_DELETED_LEFT && p.xs < 0)
-            continue # The projetile may have been removed in the original game
+            continue # The projectile may have been removed in the original game
         end
         found = false
         for pt in truth.projectiles
@@ -93,10 +73,48 @@ function check_simulation(simulation::GameState, truth::GameState)
         if !found
             println("A simulated projectile has no equivalent in the truth.")
             print_object(p, "Projectile")
-            print_all_projectiles(nothing, prev)
+            print_all_projectiles(sim_prev, truth_prev)
             print_all_projectiles(simulation, truth)
             return false
         end
+    end
+    for p in truth.projectiles
+        if (p.x > PROJ_DELETED_RIGHT && p.xs < 0) || (p.x < PROJ_DELETED_LEFT && p.xs > 0)
+            continue # The projectile is new, the simulator may not be notified about it yet
+        end
+        found = false
+        for pt in simulation.projectiles
+            if are_game_objects_equivalent(p, pt)
+                found = true
+                break
+            end
+        end
+        if !found
+            println("A projectile has no equivalent in the simulation.")
+            print_object(p, "Projectile")
+            print_all_projectiles(sim_prev, truth_prev)
+            print_all_projectiles(simulation, truth)
+            return false
+        end
+    end
+    # COLLISIONS
+    if truth.terminal
+        println("The predicted game state should be terminal!")
+        truth_proj = get_proj_in_collision(truth.player, truth.projectiles)
+        if truth_proj == nothing
+            println("No collision detected in the truth.")
+            (truth_proj,_) = nearest_projectile(player_proj_dist², truth)
+            print_object(simulation.player, "Player")
+            print_object(truth_proj, "Nearest projectile")
+            print_all_projectiles(nothing, truth)
+        else
+            println("Collision detected in the truth, but not in the simulation.")
+            (sim_proj,_) = nearest_projectile(player_proj_dist², simulation)
+            print_object(simulation.player, "Player")
+            print_boxes(sim_proj, truth_proj)
+            print_all_projectiles(simulation, truth)
+        end
+        return false
     end
     return true
 end
